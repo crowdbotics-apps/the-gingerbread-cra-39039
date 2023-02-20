5f53ef04aa56fefdd21c67d68db7ab69e01f682d
Crowdbotics <team@crowdbotics.com>
2023-02-20 19:10:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "the-gingerbread-cra-39039",
+  url: "https://the-gingerbread-cra-39039.botics.co",
+  api: "https://the-gingerbread-cra-39039.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
